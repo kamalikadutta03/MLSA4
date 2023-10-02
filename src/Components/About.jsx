dca079a8d0c57ce28e724897ed48df87023e5544
kamalikadutta03 <2104025@kiit.ac.in>
2023-10-02 23:28:05 +0530

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a third year Electronics and Communication Student. I am passionate about data science and machine learning. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Machine Learning",
+  "Web content writer",
+  "Content creator",
+  "SQL",
+  "Advanced Excel",
+  "Data Science",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am an aspiring Machine Learning Engineer. I like to solve real life problems using my skillset, one of my main goals to use my skills to help people to access tech.";
 
 const About = () => {
   return (
